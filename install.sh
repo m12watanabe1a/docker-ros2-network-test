a29cf9c0bbd2ddf0c380431ea12ff4b1aef3c710
m12watanabe1a <m12watanabe1a@gmail.com>
2022-01-20 01:16:18 +0900

    add install scripts

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Install ros2
+ROS_DISTRO="foxy"
+
+sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update && sudo apt install -y \
+  ros-${ROS_DISTRO}-desktop \
+  python3-rosdep
+
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  libbullet-dev \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-pip
+
+python3 -m pip install flake8
+
+sudo apt update && sudo apt install -y \
+  python3-pytest-cov \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+
+python3 -m pip install -U \
+  argcomplete \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest
+
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+
+sudo apt install --no-install-recommends -y \
+  libcunit1-dev
+
+sudo rosdep init
+rosdep update
+
+# Install Docker
+curl https://get.docker.com | sh \
+  && sudo systemctl --now enable docker
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
